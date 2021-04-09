549cb9ab40a6ef632772f666bbf3bc94fd670860
김진섭 <backtothefuture3030@gmail.com>
2021-04-09 21:31:17 +0900

    palindrome

--- k-palindrome.py
+++ k-palindrome.py
@@ -1,3 +1,11 @@
+
+'''
+k-palindrome 은 문자열에서 최대 k개의 문자를 제거했을 때 palindrome이 되는 문자열을 말한다.
+
+문자열 S와 정수값 K가 주어질 때 주어진 문자열이 k-palindrome일 경우 "YES", 아닐경우에는 "NO"를 출력하시오. (단, S의 최대길이는 20,000, K의 범위는 0<=K<=30)
+
+'''
+
 from itertools import combinations
 
 a = input("입력 : ") 
@@ -19,6 +27,7 @@ for i in c:
         break
 if t<1:
     print("No")
+
 
 
 
