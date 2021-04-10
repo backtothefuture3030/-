2c982db4800a20e29c028718e6874f3d9d7dd864
김진섭 <backtothefuture3030@gmail.com>
2021-04-10 22:21:23 +0900

    4sqrtnum

---
+++ 4개의 제곱수.py
@@ -0,0 +1,17 @@
+import random, math
+
+
+def root(num):
+    a= [0,0,0,0]
+    while a[0]**2 + a[1]**2 + a[2]**2 +a[3]**2 != num:
+        a[0] = int(random.randint(0,int(math.sqrt(num))))
+        a[1] = int(random.randint(0,int(math.sqrt(num))))
+        a[2] = int(random.randint(0,int(math.sqrt(num))))
+        a[3] = int(random.randint(0,int(math.sqrt(num))))
+    print(a)
+
+root(1572)
+
+
+
+
